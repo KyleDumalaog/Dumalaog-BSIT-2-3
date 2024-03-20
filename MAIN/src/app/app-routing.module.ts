dc30abc98bd4a8d281a2d205f4af41204199321e
KyleDumalaog <kylerogerdumalaog@gmail.com>
2024-03-20 17:18:58 +0800

    Assignment 2

--- MAIN/src/app/app-routing.module.ts
+++ MAIN/src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'my-custom-page',
+    loadChildren: () => import('./my-custom-page/my-custom-page.module').then( m => m.MyCustomPagePageModule)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
