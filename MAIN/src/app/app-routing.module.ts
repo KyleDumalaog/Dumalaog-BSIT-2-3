f0f33119756e1f77b85ef3d1d537a0481eb26024
KyleDumalaog <kylerogerdumalaog@gmail.com>
2024-03-27 19:09:59 +0800

    Assignment 5

--- MAIN/src/app/app-routing.module.ts
+++ MAIN/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { TabsComponent } from './tabs/tabs.component';
+import { AuthenticationService } from './authentication.service';
 
 const routes: Routes = [
   {
@@ -7,14 +9,39 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'assignment1',
-    loadChildren: () => import('./assignment1/assignment1-module').then( m => m.Assignment1Module)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
+  {
+    path: 'components',
+    loadChildren: () => import('./components/components.module').then( m => m.ComponentsPageModule)
+  },
+  {
+    path: 'tabs',
+    component: TabsComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+      },
+      {
+        path: 'components',
+        loadChildren: () => import('./components/components.module').then( m => m.ComponentsPageModule)
+      },
+    ]
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+
+
+
 ];
 
 @NgModule({
